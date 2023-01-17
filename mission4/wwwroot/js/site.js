e7d8b96c409a88779ec05e7f57e638954a83725a
Andy Graviet <graviet.andrew@gmail.com>
2023-01-17 08:49:48 -0700

    add grade calculator

--- mission4/wwwroot/js/site.js
+++ mission4/wwwroot/js/site.js
@@ -3,11 +3,36 @@
 
 // Write your JavaScript code.
 
-//document.getElementById("btnSend").addEventListener("click", function () {
-//    alert("This worked: " + $("#userName").value);
-//})
+$("#btnSend").click(function () {
+    event.preventDefault()
+    $("#finalGrade").text("")
 
-
-$("#btnSend").click(() => {
-    alert($("#userName").val())
+    var calculation = ((0.5 * $("#assignments").val()) + (0.1 * $("#groupproject").val()) + (0.1 * $("#quizzes").val()) + (0.1 * $("#midterm").val()) + (0.1 * $("#final").val()) + (0.1 * $("#intex").val()));
+    var letter = ""
+    if (calculation >= 94){
+        letter = "A  ";
+    } else if (calculation >= 90) {
+        letter = "A-  ";
+    } else if (calculation >= 87) {
+        letter = "B+  ";
+    } else if (calculation >= 84) {
+        letter = "B  ";
+    } else if (calculation >= 80) {
+        letter = "B-  ";
+    } else if (calculation >= 77) {
+        letter = "C+  ";
+    } else if (calculation >= 74) {
+        letter = "C  ";
+    } else if (calculation >= 70) {
+        letter = "C-  ";
+    } else if (calculation >= 67) {
+        letter = "D+  ";
+    } else if (calculation >= 64) {
+        letter = "D  ";
+    } else if (calculation >= 60) {
+        letter = "D-  ";
+    } else {
+        letter = "E  ";
+    }
+    $("#finalGrade").text(letter + ": " + String(calculation))
 })
