e47ddccb4ef81f166bf13b736f5257a8429460ba
Andy Graviet <graviet.andrew@gmail.com>
2023-01-16 20:59:58 -0700

    first commit

---
+++ mission4/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//document.getElementById("btnSend").addEventListener("click", function () {
+//    alert("This worked: " + $("#userName").value);
+//})
+
+
+$("#btnSend").click(() => {
+    alert($("#userName").val())
+})
