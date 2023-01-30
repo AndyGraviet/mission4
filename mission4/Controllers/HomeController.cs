b8f45fb82a0544c4ca25cd54b24ee3b00bd2635b
AndyGraviet <93163571+AndyGraviet@users.noreply.github.com>
2023-01-30 13:24:09 -0700

    add page for form

--- mission4/Controllers/HomeController.cs
+++ mission4/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace mission4.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult GradeCalculator()
         {
             return View();
         }
--- mission4/Startup.cs
+++ mission4/Startup.cs
@@ -29,31 +29,20 @@ namespace mission4
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDefaultFiles();
+
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
-            //else
-            //{
-            //    app.UseExceptionHandler("/Home/Error");
-            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            //    app.UseHsts();
-            //}
-            //app.UseHttpsRedirection();
-            //app.UseStaticFiles();
 
-            //app.UseRouting();
+            app.UseRouting();
 
-            //app.UseAuthorization();
+            app.UseAuthorization();
 
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapControllerRoute(
-            //        name: "default",
-            //        pattern: "{controller=Home}/{action=Index}/{id?}");
-            //});
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
